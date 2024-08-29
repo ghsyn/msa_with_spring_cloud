471d0f66a54b19c49638f8aba8e7022e9262af39
ghsyn <dev.demonstrate@gmail.com>
2024-08-29 15:14:51 +0900

    add routing filter config at apigateway service (route to java, not yml, and add header)

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/second")
+@Slf4j
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-service-req") String header) {
+        log.info("::>> header : {}", header);
+        return "Hello Second Service with Header Message.";
     }
 }
